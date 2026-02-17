5bb3062383c3963318831e1c7b0eecada38834b1
surbhi-codes <jhasurbhi21@gmail.com>
2026-02-17 21:03:10 +0530

    add variables

---
+++ 1_basics/02_variables.py
@@ -0,0 +1,10 @@
+# Strings   
+first_name = "Surbhi"
+favourite_food = "Momos"
+email = "surbhi@cool.com"
+
+"""
+print(f"Hello {first_name}")
+print(f"You like {favourite_food}")
+print(f"Your email is {email}")
+ """
--- 1_basics/main.py
+++
@@ -1,3 +0,0 @@
-# This is my first Python program
-print("Hello Engineer!")
-print("I like dancing and reading books.")
