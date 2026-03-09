d991a45200b6a6ffc6f7db15b28bef8c4ec02397
surbhi-codes <jhasurbhi21@gmail.com>
2026-03-09 21:23:03 +0530

    add variables

--- 1_basics/02_variables.py
+++ 1_basics/02_variables.py
@@ -2,9 +2,16 @@
 first_name = "Surbhi"
 favourite_food = "Momos"
 email = "surbhi@cool.com"
-
 """
 print(f"Hello {first_name}")
 print(f"You like {favourite_food}")
 print(f"Your email is {email}")
- """
+"""
+# Integers
+age = 22
+quantity = 3
+num_of_students = 50
+
+print(f"You're {age} years old")
+print(f"You are buying {quantity} items")
+print(f"Your class has {num_of_students} students")
