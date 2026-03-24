3cdeffe31f39d3c878eb9d64e3b3d7c260e98192
surbhi-codes <jhasurbhi21@gmail.com>
2026-03-24 18:39:19 +0530

    Add variables

--- 1_basics/02_variables.py
+++ 1_basics/02_variables.py
@@ -12,6 +12,26 @@ age = 22
 quantity = 3
 num_of_students = 50
 
-print(f"You're {age} years old")
+print(f"You're {age} years old") 
+# The f in Python stands for “formatted string” (or f-string) It lets you directly insert variables or expressions inside a string- cleanly and efficiently.
+# Without f, Python treats everything inside quotes as plain text
 print(f"You are buying {quantity} items")
 print(f"Your class has {num_of_students} students")
+
+#Float
+price = 10.99
+gpa = 9.5
+
+print(f"The price is ${price}")
+print(f"Your gpa is :{gpa}")
+
+#Boolean
+
+is_student = True
+print(f"Are you a student?: {is_student}")
+
+is_online = False
+if is_online:
+    print("You're online")
+else: 
+    print("You're offline")
