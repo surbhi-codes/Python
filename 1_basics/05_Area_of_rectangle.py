f0c45c9b8bcae18c42bfb6da4ba9e8ac6af53ec3
surbhi-codes <jhasurbhi21@gmail.com>
2026-03-25 21:16:48 +0530

    Add area of rectangle

---
+++ 1_basics/05_Area_of_rectangle.py
@@ -0,0 +1,7 @@
+# Exercise 1 - Find area of rectangle
+
+length = float(input("Enter the length: "))
+width = float(input("Enter the width: "))
+
+Area = length * width
+print(f"The area is: {Area}")
