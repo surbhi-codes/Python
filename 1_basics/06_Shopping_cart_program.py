faf8051670125543cba46412b61d1f35697147b9
surbhi-codes <jhasurbhi21@gmail.com>
2026-06-03 15:06:45 +0530

    Add shopping cart program

--- 1_basics/06_Shopping_cart_program.py
+++ 1_basics/06_Shopping_cart_program.py
@@ -1,9 +1,10 @@
 # Exercise 2 - Shopping Cart Program
 
 item = input("What item would you like to buy?: ")
-price = float(input("What is the price?: "))
+price = float(input("What is the price?: $"))
 quantity = int(input("How many would you like to buy?: "))
 
 total = price * quantity
 
-
+print(f"You have bought {quantity} X {item}")
+print(f"Your total is: ${total}")
